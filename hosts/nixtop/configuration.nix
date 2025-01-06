cf7abff88fa84b71b9199c45f79de2710440c0be
cmacwill1 <charles.macwilliams@gmail.com>
2025-01-05 19:54:31 -0700

    further discretization

--- hosts/nixtop/configuration.nix
+++ hosts/nixtop/configuration.nix
@@ -10,6 +10,7 @@
     ./hardware-configuration.nix
     inputs.home-manager.nixosModules.default
     ../../modules/nixosModules/locale.nix
+    ../../modules/nixosModules/networking.nix
   ];
  
   nix.settings.experimental-features = [
@@ -22,17 +23,6 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-
-  networking.hostName = "nixos"; 
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   # Configure keymap in X11
   services = {
