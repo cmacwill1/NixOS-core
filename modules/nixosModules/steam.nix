3fdd5eaa1dc6ae6a35b2cf1e287825f6218df992
cmacwill1 <charles.macwilliams@gmail.com>
2025-01-13 14:04:53 -0700

    trying again

--- modules/nixosModules/steam.nix
+++ modules/nixosModules/steam.nix
@@ -4,9 +4,10 @@
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
-    environment.systemPackages = with pkgs; [
-      mangohud
-    ];
-    programs.gamemode.enable = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    mangohud
+  ];
+  programs.gamemode.enable = true;
 }
