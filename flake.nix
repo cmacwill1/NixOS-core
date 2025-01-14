e0b83f4700ce907e533d1c69b34d88befba0d52b
cmacwill1 <charles.macwilliams@gmail.com>
2025-01-14 15:59:18 -0700

    typo

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
         inputs.stylix.nixosModules.stylix
       ];
     };
-    nixosConfirgurations.portable = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.portable = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = {inherit inputs;};
       modules = [
