8d6f060cb26efdfc97ba6ca55a5b2a5f1458be8f
cmacwill1 <charles.macwilliams@gmail.com>
2025-01-17 22:05:48 -0700

    typo

--- modules/nixosModules/software/stylixModule.nix
+++ modules/nixosModules/software/stylixModule.nix
@@ -10,7 +10,7 @@
     stylix = {
       enable = true;
       base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-      image = ../../wallpapers/ghibli-background-1.jpg;
+      image = ../../../wallpapers/ghibli-background-1.jpg;
     };
   };
 }
