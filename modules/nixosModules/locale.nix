70b14a94a82fe0b4da3f07c05927974b95b81eea
cmacwill1 <charles.macwilliams@gmail.com>
2025-01-17 21:10:46 -0700

    added localization to toggleable

--- modules/nixosModules/locale.nix
+++ modules/nixosModules/locale.nix
@@ -1,20 +1,27 @@
-{ ... }:
+{ lib, config, ... }:
 
 {
-  time.timeZone = "America/Denver";
+  options = {
+    locale.enable =
+      lib.mkEnableOption "general locale config";
+  };
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  config = lib.mkIf config.locale.enable {
+    time.timeZone = "America/Denver";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
   };
 }
