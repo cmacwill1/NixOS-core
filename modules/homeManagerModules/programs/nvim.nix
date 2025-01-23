fbbd38d411c7488583ab48865f064fd9d695a6f6
cmacwill1 <charles.macwilliams@gmail.com>
2025-01-23 14:26:04 -0700

    attemped nvim via nixvim

--- modules/homeManagerModules/programs/nvim.nix
+++ modules/homeManagerModules/programs/nvim.nix
@@ -7,38 +7,10 @@
   };
 
   config = lib.mkIf config.nvim.enable {
-  programs.neovim = {
-    enable = true;
-
-    viAlias = true;
-    vimAlias = true;
-
-    plugins = with pkgs.vimPlugins; [
-      bufferline-nvim
-      lualine-nvim
-      nvim-web-devicons
-      nvim-lspconfig
-      nvim-cmp
-      lspkind-nvim
-      cmp-nvim-lsp
-    ];
-    extraPackages = with pkgs; [
-      wl-clipboard
-      nixd
-      alejandra
-    ];
-    extraLuaConfig = ''
-      ${builtins.readFile ./extras/options.lua}
-      ${builtins.readFile ./plugins/lsp.lua}
-      ${builtins.readFile ./plugins/cmp.lua}
-      require("bufferline").setup({
-        options = {
-          themable = true,
-        }
-      })
-      require("lualine").setup({})
-      require("nvim-web-devicons").setup({})
-    '';
-  };
+    programs.nixvim = {
+      enable = true;
+      colorschemes.catppuccin.enable = true;
+      plugins.lualine.enable = true;
+    };
   };
 }
