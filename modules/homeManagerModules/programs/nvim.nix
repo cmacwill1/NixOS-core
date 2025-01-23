a06ae1049b3369a682235a3a6a89ddbe431ef868
cmacwill1 <charles.macwilliams@gmail.com>
2025-01-23 17:51:51 -0500

    barebones nixvim

--- modules/homeManagerModules/programs/nvim.nix
+++ modules/homeManagerModules/programs/nvim.nix
@@ -9,10 +9,23 @@
   config = lib.mkIf config.nvim.enable {
     programs.nixvim = {
       enable = true;
+
+      vimAlias = true;
+      viAlias = true;
+
       plugins = {
         lualine.enable = true;
 	bufferline.enable = true;
 	web-devicons.enable = true;
+      };
+      
+      globals.mapleader = " ";
+
+      opts = {
+        number = true;
+        relativenumber = true;
+
+        shiftwidth = 2;
       };
     };
   };
