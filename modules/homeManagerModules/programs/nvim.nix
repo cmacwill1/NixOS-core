c80e496ced7f162002e198f7f57568e6a91c295a
cmacwill1 <charles.macwilliams@gmail.com>
2025-01-30 23:09:57 -0500

    added LSP

--- modules/homeManagerModules/programs/nvim.nix
+++ modules/homeManagerModules/programs/nvim.nix
@@ -17,6 +17,38 @@
         lualine.enable = true;
 	bufferline.enable = true;
 	web-devicons.enable = true;
+	lsp = {
+	  enable = true;
+	  servers = {
+	    nixd.enable = true;
+	    pyright.enable = true;
+	  };
+	};
+	cmp = {
+          enable = true;
+          autoEnableSources = true;
+          settings = {
+            sources = [
+              { name = "nvim_lsp"; }
+              { name = "luasnip"; }
+              { name = "buffer"; }
+              { name = "path"; }
+            ];
+            mapping = {
+              "<CR>" = "cmp.mapping.confirm({ select = true })";
+              "<Down>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+              "<Up>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+              "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+              "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+              "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+              "<C-f>" = "cmp.mapping.scroll_docs(4)";
+              "<C-Space>" = "cmp.mapping.complete()";
+            };
+          };
+        };
+        cmp-nvim-lsp.enable = true;
+        cmp-buffer.enable = true;
+        cmp-path.enable = true;
       };
       
       clipboard.providers.wl-copy.enable = true;
