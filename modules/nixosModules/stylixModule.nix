a4e18a685b3fdcdebee94a4a1409f4fd14e40ff5
cmacwill1 <charles.macwilliams@gmail.com>
2025-02-06 15:05:29 -0500

    testing

--- modules/nixosModules/stylixModule.nix
+++ modules/nixosModules/stylixModule.nix
@@ -12,26 +12,14 @@
       base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-soft.yaml";
       image = ../../wallpapers/gruvbox.png;
       fonts = {
-	serif = {
-	  package = pkgs.dejavu_fonts;
-	  name = "DejaVu Serif";
-	};
-
-	sansSerif = {
-	  package = pkgs.dejavu_fonts;
-	  name = "DejaVu Sans";
-	};
-
 	monospace = {
-	  package = pkgs.jetbrains-mono;
+	  package = pkgs.nerd-fonts.jetbrains-mono;
 	  name = "JetBrains Mono";
 	};
-
-	emoji = {
-	  package = pkgs.noto-fonts-emoji;
-	  name = "Noto Color Emoji";
+	serif = config.stylix.fonts.monospace;
+	sansSerif = config.stylix.fonts.monospace;
+	emoji = config.stylix.fonts.monospace;
 	};
       };
     };
-  };
 }
