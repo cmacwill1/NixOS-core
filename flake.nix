fdb82dc96d47e007e73978c45f27ebacad795565
cmacwill1 <charles.macwilliams@gmail.com>
2025-02-14 13:06:50 -0500

    added firefox addons flake to flake

--- flake.nix
+++ flake.nix
@@ -3,15 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    
     stylix.url = "github:danth/stylix";
+    
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
   outputs = { nixpkgs, ... }@inputs: {
