cd78eca00581648b99b008fec6285c2c49d8a250
cmacwill1 <charles.macwilliams@gmail.com>
2025-02-16 21:24:32 -0500

    removed firefox showing recs in home page

--- modules/homeManagerModules/firefox.nix
+++ modules/homeManagerModules/firefox.nix
@@ -72,6 +72,11 @@
 	  "identity.fxaccounts.enabled" = false;
 	  "signon.rememberSignons" = false;
 	  "extensions.autoDisableScopes" = 0;
+	  "browser.newtabpage.activity-stream.feeds.topsites" = false;
+	  "browser.newtabpage.activity-stream.showWeather" = false;
+	  "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+	  "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
+	  "browser.newtabpage.activity-stream.default.sites" = "";
 	};
 
 	extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
