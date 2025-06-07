525edf49a3583f80a83d660ad07b742caf1cc19c
cmacwill1 <charles.macwilliams@gmail.com>
2025-06-07 00:57:29 -0400

    kitty tab style

--- modules/homeManagerModules/kitty.nix
+++ modules/homeManagerModules/kitty.nix
@@ -9,12 +9,13 @@
   config = lib.mkIf config.kitty.enable {
   programs.kitty = {
     enable = true;
+    shellIntegration.enableFishIntegration = true;
     settings = {
       cursor_shape = "beam";
       cursor_blink_interval = 0.5;
       cursor_stop_blinking_after = 0;
       confirm_os_window_close = 0;
-      tab_bar_style = "fade";
+      tab_bar_style = "powerline";
       enable_audio_bell = "no";
       };
   };
