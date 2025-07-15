201f2870ce01762867cd8ae1873969eb510fcaf6
cmacwill1 <charles.macwilliams@gmail.com>
2025-07-14 20:07:25 -0400

    somewhat functional rmpc config

--- modules/homeManagerModules/rmpc.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }:
-
-{
-  programs.rmpc = {
-    enable = true;
-  };
-}
---
+++ modules/homeManagerModules/rmpc/default.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }:
+
+{
+  programs.rmpc = {
+    enable = true;
+  };
+  home.file.".config/rmpc/config.ron".source = ./config.ron;
+  home.file.".config/rmpc/themes/theme.ron".source = ./theme.ron;
+}
