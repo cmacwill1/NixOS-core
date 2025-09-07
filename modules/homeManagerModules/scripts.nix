d8646b69a5c987af8af474f8b33eaf577170a2a9
cmacwill1 <charles.macwilliams@gmail.com>
2025-09-07 00:25:33 -0400

    readme

--- modules/homeManagerModules/scripts.nix
+++ modules/homeManagerModules/scripts.nix
@@ -9,7 +9,6 @@
   config = lib.mkIf config.scripts.enable {
   home.packages = [
     (import ../../scripts/current-generation.nix {inherit pkgs; })
-    (import ../../scripts/purdue-vpn.nix {inherit pkgs; })
     (import ../../scripts/audio-window.nix {inherit pkgs; })
     (import ../../scripts/network-window.nix {inherit pkgs; })
     (import ../../scripts/yazi-window.nix {inherit pkgs; })
--- scripts/purdue-vpn.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs }:
-
-pkgs.writeShellScriptBin "purdue-vpn" ''
-  sudo openconnect --protocol=anyconnect https://webvpn2.purdue.edu
-''
