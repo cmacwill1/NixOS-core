0bf5501703438681698069ed03cb319e1534b91e
cmacwill1 <charles.macwilliams@gmail.com>
2025-11-24 23:56:16 -0500

    molten keymaps

--- modules/homeManagerModules/nixvim/nixvim.nix
+++ modules/homeManagerModules/nixvim/nixvim.nix
@@ -294,6 +294,51 @@
           desc = "Move focus to the upper window";
         };
       }
+      {
+        mode = "n";
+        key = "<localleader>mi";
+        action = ":MoltenInit<CR>";
+        options = {
+          silent = true;
+          desc = "Initialize the plugin";
+        };
+      }
+      {
+        mode = "n";
+        key = "<localleader>e";
+        action = ":MoltenEvaluateOperator<CR>";
+        options = {
+          silent = true;
+          desc = "run operator selection";
+        };
+      }
+      {
+        mode = "n";
+        key = "<localleader>rl";
+        action = ":MoltenEvaluateLine<CR>";
+        options = {
+          silent = true;
+          desc = "evaluate line";
+        };
+      }
+      {
+        mode = "n";
+        key = "<localleader>rr";
+        action = ":MoltenReevaluateCell<CR>";
+        options = {
+          silent = true;
+          desc = "re-evaluate cell";
+        };
+      }
+      {
+        mode = "v";
+        key = "<localleader>r";
+        action = ":<C-u>MoltenEvaluateVisual<CR>gv";
+        options = {
+          silent = true;
+          desc = "evaluate visual selection";
+        };
+      }
     ];
 
     # https://nix-community.github.io/nixvim/NeovimOptions/autoGroups/index.html
@@ -343,6 +388,34 @@
       };
       nvim-tree = {
         enable = true;
+      };
+      image = {
+        enable = true;
+      };
+      molten = {
+        enable = true;
+        settings = {
+          auto_image_popup = false;
+          auto_init_behavior = "init";
+          auto_open_html_in_browser = false;
+          auto_open_output = true;
+          cover_empty_lines = false;
+          copy_output = false;
+          enter_output_behavior = "open_then_enter";
+          image_provider = "image.nvim";
+          output_crop_border = true;
+          output_virt_lines = false;
+          output_win_border = [ "" "━" "" "" ];
+          output_win_hide_on_leave = true;
+          output_win_max_height = 99999;
+          output_win_max_width = 99999;
+          save_path.__raw = "vim.fn.stdpath('data')..'/molten'";
+          tick_rate = 500;
+          use_border_highlights = false;
+          limit_output_chars = 10000;
+          wrap_output = false;
+          virt_text_output = true;
+        };
       };
       render-markdown = {
         enable = true;
